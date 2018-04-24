3bc76302747bc55cb6213911c0b6e650de7f488a
Ariton84 <vanver@gmail.com>
2018-04-24 11:03:43 -0700

    Loads initial News application

---
+++ CI-sp18/application/views/news/create.php
@@ -0,0 +1,26 @@
+<?php
+//applications/views/news/create.php
+
+$this->load->view($this->config->item('theme') . 'header');
+
+?>
+
+<? echo validation_errors(); ?>
+
+<? echo form_open('news/create'); ?>
+
+    <label for="title">Title</label>
+    <input type="input" name="title" /><br />
+
+    <label for="text">Text</label>
+    <textarea name="text"></textarea><br />
+
+    <input type="submit" name="submit" value="Create news item" />
+
+</form>
+
+<?php 
+ 
+$this->load->view($this->config->item('theme') . 'footer');
+
+?>
